b6cef887533f8facf72ead59a7ceb91bf58ca8d3
forDecember <1921567337@qq.com>
2023-09-14 20:16:05 +0800

    update: 初步实现web端显示

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"testCourse/data"
-	"testCourse/utils"
 	"testCourse/web"
 )
 
 func main() {
-
-	utils.Info(data.GetValidCourse(data.Plan.ChildrenNode[2]))
 	web.Test()
 
 }
